737383c90dce91ecad48fa797762e2175d53a627
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2026-05-28 10:18:53 -0300

    Atualizando as funções

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,23 +11,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Força a substituição das variáveis do Render que estão vindo erradas
+        putenv('DB_DATABASE=tasknow');
+        putenv('DB_USERNAME=tasknow');
+        putenv('DB_PASSWORD=kz8VPsV22yP7NHd0xAkD2841WLV3d1mf');
+        putenv('DB_HOST=dpg-d8c2f89kh4rs738gugi0-a.ohio-postgres.render.com');
+        putenv('DB_PORT=5432');
+        putenv('DB_SSLMODE=require');
     }
 
     /**
      * Bootstrap any application services.
      */
-  public function boot(): void
-{
-    if (env('APP_ENV') === 'production') {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-        
-        // Garante a migração automática das tabelas para o banco 'tasknow'
-        try {
-            \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        } catch (\Exception $e) {
-            // Evita que o app quebre se o banco estiver ocupado
-        }
+    public function boot(): void
+    {
+        //
     }
-}
 }
